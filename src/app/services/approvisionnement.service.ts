a7b83c7781835fd2cf1a33b6a9e13306f8ae3011
rijalseydinaissa <issadiol99@gmail.com>
2025-05-27 12:44:04 +0000

    APPRO

---
+++ src/app/services/approvisionnement.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject,Observable,tap } from 'rxjs';
+import { ApiService } from './api.service';
+
+// approvisionnement.model.ts
+export interface Approvisionnement {
+  id: number;
+  code: string;
+  dateCommande: Date;
+  dateLivraisonPrevue?: Date;
+  dateLivraisonEffective?: Date;
+  statut: 'EN_ATTENTE' | 'CONFIRME' | 'EXPEDIE' |'LIVRE_COMPLETEMENT' | 'LIVRE_PARTIELLEMENT' | 'ANNULE';
+  montantTotal: number;
+  observations?: string;
+  fournisseur: Fournisseur;
+  produits: ApproProduit[];
+}
+
+export interface ApproProduit {
+  id: number;
+  produit: Produit;
+  quantiteCommandee: number;
+  quantiteLivree: number;
+  prixUnitaire: number;
+  sousTotal: number;
+}
+interface Produit {
+  id: number;
+  nom: string;
+  code:string;
+  prix: number;
+  prixAchat: number;
+  quantite: number;
+  categorie: CategorieResponse;
+  image: string | null;
+  statut: string;
+}
+export interface CategorieResponse {
+  id: number;
+  nom: string;
+}
+
+export interface Fournisseur {
+  id: number;
+  nom: string;  
+  adresse: string;
+  email: string;
+  telephone: string;
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class ApprovisionnementService {
+  private endpoints= "/approvisionnements";  
+  private approvisionnementsSubject = new BehaviorSubject<Approvisionnement[]>([]);
+  approvisionnements = this.approvisionnementsSubject.asObservable();
+
+  constructor(private apiService: ApiService) {
+    this.loadApprovisionnements();
+   }
+
+  private loadApprovisionnements() {
+    this.apiService.get<Approvisionnement[]>(this.endpoints).subscribe(data=>{
+      this.approvisionnementsSubject.next(data);
+      console.log(data);
+    });
+  }
+  public getApprovisionnements(): Observable<Approvisionnement[]> {
+    return this.approvisionnements;
+  }
+  public createApprovisionnement(approvisionnementData: Approvisionnement): Observable<Approvisionnement> {
+    return this.apiService.post<Approvisionnement>(`${this.endpoints}`, approvisionnementData)
+      .pipe(
+        tap((newApprovisionnement: Approvisionnement) => {
+          const currentApprovisionnements = this.approvisionnementsSubject.value;
+          this.approvisionnementsSubject.next([...currentApprovisionnements, newApprovisionnement]);
+        })
+      );
+  }
+  public updateApprovisionnement(id: number, approvisionnementData: Approvisionnement): Observable<Approvisionnement> {
+    return this.apiService.put<Approvisionnement>(`${this.endpoints}/${id}`, approvisionnementData)
+      .pipe(
+        tap((updatedApprovisionnement: Approvisionnement) => {
+          const currentApprovisionnements = this.approvisionnementsSubject.value;
+          const updatedApprovisionnements = currentApprovisionnements.map(approvisionnement =>
+            approvisionnement.id === id ? updatedApprovisionnement : approvisionnement
+          );
+          this.approvisionnementsSubject.next(updatedApprovisionnements);
+        })
+      );  
+  }
+  public deleteApprovisionnement(id:number) {
+    this.apiService.delete<Approvisionnement>(`${this.endpoints}/${id}`).pipe(
+      tap(() => {
+        const currentApprovisionnements = this.approvisionnementsSubject.getValue();
+        this.approvisionnementsSubject.next(currentApprovisionnements.filter(approvisionnement => approvisionnement.id !== id));
+      })
+    );
+  }
+}
